faaf9801872fe0e6a6a23eff48c40608ac9ea582
Nahuel <nhlgrc@hotmail.com>
2023-08-03 17:01:40 -0300

    aaa

--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -8,7 +8,10 @@ import { User } from "../models/user";
  */
 export const loadUsersByPage = async (page = 1) => {
 
-    const url = `${ import.meta.env.VITE_BASE_URL}/users?_page=${ page }`;
+    const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;
+    //const url = `http://localhost:3001/users?_page=${ page }`;
+    
+    console.log(url);
     const res = await fetch(url);
     const data = await res.json();
 
