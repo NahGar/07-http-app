03dd9c8a92caf046392637f714e89c4c390df383
Nahuel Garcia <nhlgrc@hotmail.com>
2023-08-04 17:11:53 -0300

    cambios

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,22 +1,50 @@
+import { User } from '../../models/user';
 import './render-modal.css';
 import modalHtml from './render-modal.html?raw';
+import { getUserById } from "../../use-cases/get-user-by-id";
 
 let modal, form;
+//se utiliza para no perder campos que no se mantienen, como por 
+//ejemplo: gender
+let loadedUser = {}; //objeto vacío
 
-
-export const showModal = () => {
+/**
+ * 
+ * @param {String|Number} id 
+ */
+export const showModal = async ( id ) => {
     modal?.classList.remove('hide-modal');
+    loadedUser = {}; //objeto vacío
+
+    if(!id) return;
+    const user = await getUserById( id );
+    setFormValues( user );
 }
 
 export const hideModal = () => {
     modal?.classList.add('hide-modal');
+    //limpia los valores cargados
+    form?.reset();
+}
+
+/**
+ * 
+ * @param {User} user 
+ */
+const setFormValues = ( user ) => {
+    form.querySelector('[name="firstName"]').value = user.firstName;
+    form.querySelector('[name="lastName"]').value = user.lastName;
+    form.querySelector('[name="balance"]').value = user.balance;
+    form.querySelector('[name="isActive"]').checked = user.isActive;
+    loadedUser = user;
 }
 
 /**
  * 
  * @param {HTMLDivElement} element 
+ * @param {(userLike)=>Promise<void>} callback
  */
-export const renderModal = ( element ) => {
+export const renderModal = ( element, callback ) => {
 
     if( modal ) return;
 
@@ -27,18 +55,18 @@ export const renderModal = ( element ) => {
     form = modal.querySelector('form');
 
     modal.addEventListener('click', (event) => {
-        console.log(event.target);
+        
         if(event.target.className === 'modal-container') {
             hideModal();
         }
     });
 
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
         //para evitar el POST
         event.preventDefault();
 
         const formData = new FormData( form );
-        const userLike = {};
+        const userLike = { ...loadedUser };
 
         //for (const iterator of formData) {
         for (const [key, value] of formData) {
@@ -57,6 +85,10 @@ export const renderModal = ( element ) => {
             userLike[key] = value;
 
         }
+
+        await callback( userLike );
+
+        hideModal();
     });
 
     element.append( modal );
